0d4a9bffa537e14db46fbc4561b4e60caea7153e
Jaseem P <jaseemp718@gmail.com>
2025-09-23 23:32:20 +0530

    refactored the code

--- Colliders-Gizmos/Assets/Scripts/AllRigidBody.cs
+++ Colliders-Gizmos/Assets/Scripts/AllRigidBody.cs
@@ -29,55 +29,10 @@ public class AllRigidBody : MonoBehaviour
 
     }
 
-    // private void FindAllCollider()
-    // {
-    //     Collider[] collidersList = FindObjectsOfType<Collider>();
-    //     foreach (var collider in collidersList)
-    //     {
-    //         if (collider.isTrigger)
-    //         {
-    //             Handles.Label(collider.transform.position + Vector3.up *2.5f,"Has IsTrigger");
-    //         }
-
-    //         if (collider is BoxCollider box)
-    //             {
-    //                 Gizmos.color = Color.blue;
-    //                 Gizmos.DrawWireCube(box.bounds.center, box.bounds.size);
-    //             }
-    //             else if (collider is SphereCollider sphere)
-    //             {
-    //                 Gizmos.color = Color.yellow;
-    //                 Gizmos.DrawWireSphere(sphere.bounds.center, sphere.radius);
-    //             }
-    //             else if (collider is CapsuleCollider capsule)
-    //             {
-    //                 Gizmos.color = Color.brown;
-    //                 // center position
-    //                 Vector3 center = capsule.transform.position + capsule.center;
-    //                 float radius = capsule.radius;
-    //                 float height = capsule.height;
-
-    //                 // cylinder part
-    //                 Vector3 cylinderHeight = Vector3.up * (height - 2 * radius);
-    //                 Gizmos.DrawCube(center, new Vector3(radius * 2, cylinderHeight.y, radius * 2));
-
-    //                 // top cap
-    //                 Gizmos.DrawSphere(center + Vector3.up * (cylinderHeight.y / 2), radius);
-    //                 // bottom cap
-    //                 Gizmos.DrawSphere(center - Vector3.up * (cylinderHeight.y / 2), radius);
-    //             }
-    //             Handles.Label(collider.transform.position + Vector3.up * 3f, collider.tag);
-
-
-    //     }
-
-    // }
-
 
     private void OnDrawGizmos()
     {
         FindAllRigidBody();
-        //FindAllCollider();
     }
 
 }
